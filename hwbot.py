5a6ca0a82bfdf8118e95d4a22d935c4244d0b711
Kevin Stoner <stonek4rpi@gmail.com>
2017-03-15 17:21:32 -0600

    fixed some issues with prints

--- hwbot.py
+++ hwbot.py
@@ -1,3 +1,11 @@
+'''
+HWBOT
+v1.0
+
+Organizing and creating hw has never been so easy!
+
+Kevin Stoner
+'''
 ### IMPORTS ###
 from math import *
 import os
@@ -12,84 +20,101 @@ from creator import Creator
 
 ### FUNCTIONS ###
 def setup_logging():
-    """ Setup the logging library """
+    ''' Setup the logging library '''
     logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
     logging.debug("Logging Started...")
 
 def display_options(which):
+    ''' Allow the user to input a new command '''
     parser = argparse.ArgumentParser()
-    first_arg = ["create","edit","exit"]
+    first_arg = ["create", "edit", "exit"]
     second_arg = []
 
-    print ("Current Directory: "+os.getcwd())
-    for f in os.listdir("./"):
-        print (f)
-    print ("")
+    print "Current Directory: " + os.getcwd()
+    for proj_file in os.listdir("./"):
+        print proj_file
+    print ""
 
-    if (which == 1):
+    if which == 1:
         second_arg += ["project"]
-    if (which >= 2):
+    elif which >= 2:
         first_arg += ["compile", "delete", "add"]
         second_arg += ["q", "file"]
-        if (which >= 3):
+        if which >= 3:
             second_arg += ["sq"]
-        if (which >= 4):
+        else:
             second_arg += ["ssq"]
 
-    parser.add_argument("command", type=str, choices=first_arg, help="the command to be run")
-    parser.add_argument("c_type", nargs='?', type=str, choices=second_arg, help="the type to perform the command on")
-    parser.add_argument("name", nargs='?', type=str, help="define the folder name, file number (ex: for Question 1, enter '1'), or path to file")
+    parser.add_argument("command",
+                        type=str,
+                        choices=first_arg,
+                        help="the command to be run")
+    parser.add_argument("c_type",
+                        nargs='?',
+                        type=str,
+                        choices=second_arg,
+                        help="the type to perform the command on")
+    parser.add_argument("name",
+                        nargs='?',
+                        type=str,
+                        help="""define the folder name, file number
+                        (ex: for Question 1, enter '1'), or path to file""")
     while True:
         try:
             input_args = input(" ~hwbot~: ").split(" ")
             output = parser.parse_args(input_args)
             break
         except Exception as e:
-            print (e)
+            print e
 
     return output
 
 def navigate_to(name):
+    ''' Navigate to a parent file called name '''
     while True:
-        cd = os.getcwd().split(os.sep)[-1]
-        if (cd == name or cd.split('_')[0] == name):
-            mx = 0
-            for dr in os.listdir("./"):
-                if (os.path.isdir("./"+dr)):
-                    num = int(dr.split('_')[1])
-                    if num > mx:
-                        mx = num
-            return mx + 1
+        curr_dir = os.getcwd().split(os.sep)[-1]
+        if curr_dir == name or curr_dir.split('_')[0] == name:
+            amax = 0
+            for adir in os.listdir("./"):
+                if os.path.isdir("./" + adir):
+                    num = int(adir.split('_')[1])
+                    if num > amax:
+                        amax = num
+            return amax + 1
         else:
             os.chdir("../")
 
 def get_file_number():
-    mx = 0
-    for dr in os.listdir("./"):
-        if (os.path.isfile("./"+dr)):
-            num = int(dr.split('_')[1].split('.')[0])
-            if num > mx:
-                mx = num
-    return mx + 1
+    ''' Get the next usable file number in a directory and return it '''
+    amax = 0
+    for adir in os.listdir("./"):
+        if os.path.isfile("./" + adir):
+            num = int(adir.split('_')[1].split('.')[0])
+            if num > amax:
+                amax = num
+    return amax + 1
 
 def get_file(fnum):
-    for dr in os.listdir("./"):
-        if (os.path.isfile("./"+dr)):
-            num = int(dr.split('_')[1].split('.')[0])
-            if (num == fnum):
-                return dr
+    ''' Get a file by number '''
+    for adir in os.listdir("./"):
+        if os.path.isfile("./" + adir):
+            num = int(adir.split('_')[1].split('.')[0])
+            if num == fnum:
+                return adir
     return None
 
-def compile(compiler, exclude):
+def md_compile(compiler, exclude):
+    ''' Compile the assignment using the compile class '''
     file_list = []
     dir_list = []
-    for dr in os.listdir():
-        if (dr not in exclude):
-            if (os.path.isfile("./"+dr)):
-                file_list.append("./"+dr)
+    for adir in os.listdir():
+        if adir not in exclude:
+            if os.path.isfile("./" + adir):
+                file_list.append("./" + adir)
             else:
-                dir_list.append("./"+dr)
+                dir_list.append("./" + adir)
     print (file_list, dir_list)
     file_list.sort(key=lambda a: a.split("_")[1].split(".")[0])
     for afile in file_list:
@@ -103,6 +128,7 @@ def compile(compiler, exclude):
         os.chdir("../")
 
 def parse_input(creator, command, option):
+    ''' Parse the commands that were input '''
     if command.command == "create":
         if command.c_type == "project":
             creator.set_project_name(command.name)
@@ -185,6 +211,7 @@ def parse_input(creator, command, option):
 
 ### MAIN ###
 def main():
+    ''' Main '''
     option = 1
     creator = Creator()
     command = display_options(option)
